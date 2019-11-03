7a0c2781df6e7ae48219a77d52523397d497e298
Thomas Wang <thomas-wang@Thomass-MacBook-Pro.local>
2019-11-03 13:47:14 -0800

    background image

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ import NewSession from "./routes/NewSession/new-session";
 import Checkin from "./routes/Checkin/Checkin";
 import Navbar from "./Components/Navbar/Navbar";
 import CurrentSession from "./routes/CurrentSession/current-session";
+import "./App.css";
 
 const App = () => (
   <Router>
-    <div
-      id="Background"
-      style={{
-        background: "url(./Components/web.png)",
-        backgroundSize: "100% 800px",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "0px -150px"
-      }}
-    >
+    <div id="Background">
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
