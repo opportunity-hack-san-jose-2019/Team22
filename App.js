cca630edd66edb0a4605f6b82501091cb795d64f
elizabeth-wong-ab <ewong586@gmail.com>
2019-11-02 17:56:27 -0700

    Added Routes

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./routes/Home";
+import NewSession from "./routes/new-session";
+import Checkin from "./routes/Checkin";
+
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path='/new-session' component={NewSession}/>
+      <Route path='/checkin' component={Checkin}/>
     </Switch>
   </Router>
 );
 
 export default App;
+
+
---
+++ routes/new-session.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NewSession extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>new session</div>
+            </div>
+        );
+    }
+}
+
+export default NewSession;
